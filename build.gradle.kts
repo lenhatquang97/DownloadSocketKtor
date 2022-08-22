e3a01ab5d61b482b02f206c60c58b3d4f18fcaf8
quangln2 <quangln2@vng.com.vn>
2022-08-22 11:21:02 +0700

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,18 +5,17 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.7.10"
-    id("io.ktor.plugin") version "2.1.0"
 }
 
 group = "com.example"
 version = "0.0.1"
 application {
+
     mainClass.set("com.example.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
-
 tasks {
     create("stage").dependsOn("installDist")
 }
